5f90d24dfffefdbc52bf798a2ec57874c68b5ff4
Janggun Lee <leesisi123@naver.com>
2021-12-06 04:52:10 +0900

    Add demo script

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import konglog
+
+def main():
+    print("Welcome to Konglog mini example.")
+    while(True):
+        print("Type an English word you want to translate.")
+        eng_word = input("English word, or N to exit: ")
+        if not eng_word.lower().strip() == 'n':
+            print("Thanks for trying Konglog!")
+            exit(0)
+        print("Translating to Konglish. This may take a bit...")
+        kongs = konglog.eng_to_kong(eng_word)
+        print(f"{eng_word} is translated into {kongs}")
+        print()
+
+if __name__ == "__main__":
+    main()
